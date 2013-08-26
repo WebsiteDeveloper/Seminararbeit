5a2b1e4a78d1f138d54bfd01b0f8ed95c0c9cec5
Bernhard Sirlinger <bernhard@sirlinger.de>
2013-08-26 14:48:16 +0200

    Refactor Vectors

---
+++ src/mss/util/Vektor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mss.util;
+
+/**
+ *
+ * @author Bernhard Sirlinger
+ */
+public abstract class Vektor {
+
+    /**
+     *
+     * @return
+     */
+    public abstract double betrag();
+
+    /**
+     *
+     */
+    public abstract void normalisieren();
+}
--- src/mss/util/Vektor3D.java
+++ src/mss/util/Vektor3D.java
@@ -8,40 +8,38 @@ package mss.util;
  *
  * @author Bernhard Sirlinger
  */
-public class Vektor2D extends Vektor {
+public class Vektor3D extends Vektor {
     private double x;
     private double y;
-    
-    public Vektor2D() {
-        this.x = 0.0;
-        this.y = 0.0;
+    private double z;
+
+    /**
+     *
+     */
+    public Vektor3D() {
+        this(0.0, 0.0, 0.0);
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param z
+     */
+    public Vektor3D(int x, int y, int z) {
+        this((double)x, (double)y, (double)z);
     }
     
     /**
      *
      * @param x
      * @param y
+     * @param z
      */
-    public Vektor2D(int x, int y) {
-        this((double)x, (double)y);
-    }
-    
-    /**
-     *
-     * @param x
-     * @param y
-     */
-    public Vektor2D(double x, double y) {
+    public Vektor3D(double x, double y, double z) {
         this.x = x;
         this.y = y;
-    }
-    
-    /**
-     *
-     * @param vec
-     */
-    public Vektor2D(Vektor2D vec) {
-        this(vec.getX(), vec.getY());
+        this.z = z;
     }
 
     /**
@@ -62,6 +60,14 @@ public class Vektor2D extends Vektor {
 
     /**
      *
+     * @return
+     */
+    public double getZ() {
+        return z;
+    }
+
+    /**
+     *
      * @param x
      */
     public void setX(double x) {
@@ -75,17 +81,26 @@ public class Vektor2D extends Vektor {
     public void setY(double y) {
         this.y = y;
     }
+
+    /**
+     *
+     * @param z
+     */
+    public void setZ(double z) {
+        this.z = z;
+    }
     
     @Override
     public double betrag() {
-        return Math.sqrt(Math.pow(this.x, 2.0) + Math.pow(this.y, 2.0));
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
     }
-    
+
     @Override
     public void normalisieren() {
         double betrag = this.betrag();
         this.x /= betrag;
         this.y /= betrag;
+        this.z /= betrag;
     }
     
     /**
@@ -94,8 +109,8 @@ public class Vektor2D extends Vektor {
      * @param vec2
      * @return
      */
-    public static Vektor2D add(Vektor2D vec1, Vektor2D vec2) {
-        Vektor2D erg = new Vektor2D(vec1.getX() + vec2.getX(), vec1.getY() + vec2.getY());
+    public static Vektor3D add(Vektor3D vec1, Vektor3D vec2) {
+        Vektor3D erg = new Vektor3D(vec1.getX() + vec2.getX(), vec1.getY() + vec2.getY(), vec1.getZ() + vec2.getZ());
         
         return erg;
     }
@@ -106,8 +121,8 @@ public class Vektor2D extends Vektor {
      * @param vec
      * @return
      */
-    public static Vektor2D skalarMultiply(double skalar, Vektor2D vec) {
-        Vektor2D erg = new Vektor2D(skalar * vec.getX(), skalar * vec.getY());
+    public static Vektor3D skalarMultiply(double skalar, Vektor3D vec) {
+        Vektor3D erg = new Vektor3D(skalar * vec.getX(), skalar * vec.getY(), skalar * vec.getZ());
         
         return erg;
     }
