55477acdfdb76d2d41e8c472d73cf21a75661740
Bernhard Sirlinger <bernhard@sirlinger.de>
2013-09-02 16:12:54 +0200

    Refactor Rechenmodul

--- src/mss/integratoren/Rechenmodul.java
+++ src/mss/integratoren/Rechenmodul.java
@@ -11,101 +11,122 @@ import mss.Observable;
 import mss.Observer;
 import mss.util.Notifications;
 import mss.util.Planet;
+import mss.util.Util;
 import mss.util.Vektor2D;
 
 /**
  *
- * @author Admin
+ * @author Bernhard Sirlinger
  */
 public class Rechenmodul implements Observer, Observable, Runnable {
     private final HashMap<String, Observer> observers = new HashMap<>();
-    
+
     private final ArrayList<Rechner> integratoren = new ArrayList<>();
     private Integratoren integrator;
     private double deltaT;
-    
+
     public Rechenmodul(double deltaT) {
         this.integrator = Integratoren.RUNGE_KUTTA_KLASSISCH;
         this.deltaT = deltaT;
         this.addIntegratoren();
     }
-    
+
     public Rechenmodul(Integratoren integrator, double deltaT) {
         this.integrator = integrator;
         this.deltaT = deltaT;
         this.addIntegratoren();
     }
-    
+
     private void addIntegratoren() {
         this.integratoren.add(new Euler());
         this.integratoren.add(new Leapfrog());
         this.integratoren.add(new RungeKuttaKlassisch());
     }
-    
-    public void rechenschritt(ArrayList<Planet> planeten) {
+
+    public ArrayList<ArrayList<Planet>> computeUntilT(double deltaT, double maxT, ArrayList<Planet> startPlanets) {
+        this.deltaT = deltaT;
+        int index = 0;
+
+        ArrayList<ArrayList<Planet>> erg = new ArrayList<>((int) (maxT / deltaT));
+        erg.add(this.rechenschritt(startPlanets));
+        for (double t = deltaT; t <= maxT; t += deltaT) {
+            erg.add(this.rechenschritt(erg.get(index)));
+
+            String collisions = Util.findCollisions(erg.get(index + 1));
+            if (!collisions.isEmpty()) {
+                System.out.println(collisions);
+                return erg;
+            }
+            index++;
+        }
+
+        return erg;
+    }
+
+    public ArrayList<Planet> rechenschritt(ArrayList<Planet> planeten) {
         int size = planeten.size();
         ArrayList<Planet> ergs = new ArrayList<>(size);
-        
-        for(int i = 0; i < size; i++) {
+
+        for (int i = 0; i < size; i++) {
             Planet temp = planeten.get(i);
             Vektor2D v = temp.getV(),
-                     deltaCoords;
-            
-            for(int j = 0; j < size; j++) {
-                if(i != j) {
+                    deltaCoords;
+
+            for (int j = 0; j < size; j++) {
+                if (i != j) {
                     v = Vektor2D.add(v, this.getDeltaV(planeten.get(j), temp));
                 }
             }
-            
+
             deltaCoords = this.getDeltaCoords(v);
-            ergs.add(new Planet(temp.getLabel(), new Vektor2D(temp.getCoords().getX() + deltaCoords.getX(), temp.getCoords().getY() + deltaCoords.getY()), temp.getMass(), temp.getRadix(), (Vektor2D)v.clone(), temp.getColor()));
+            ergs.add(new Planet(temp.getLabel(), new Vektor2D(temp.getCoords().getX() + deltaCoords.getX(), temp.getCoords().getY() + deltaCoords.getY()), temp.getMass(), temp.getRadix(), (Vektor2D) v.clone(), temp.getColor()));
         }
-        
-        this.sendPlanetsToObservers(Notifications.RESULT, ergs);
+
+        return ergs;
     }
-    
+
     private Vektor2D getDeltaCoords(Vektor2D v) {
         switch (this.integrator) {
             case EULER:
-                Euler euler = (Euler)this.integratoren.get(Integratoren.EULER.ordinal());
+                Euler euler = (Euler) this.integratoren.get(Integratoren.EULER.ordinal());
                 Vektor2D coordsE = euler.getNewX(v, this.deltaT);
                 return coordsE;
             case LEAPFROG:
-                Leapfrog leapfrog = (Leapfrog)this.integratoren.get(Integratoren.LEAPFROG.ordinal());
+                Leapfrog leapfrog = (Leapfrog) this.integratoren.get(Integratoren.LEAPFROG.ordinal());
                 Vektor2D coordsL = leapfrog.getNewX(v, this.deltaT);
                 return coordsL;
             case RUNGE_KUTTA_KLASSISCH:
-                RungeKuttaKlassisch rungekutta = (RungeKuttaKlassisch)this.integratoren.get(Integratoren.RUNGE_KUTTA_KLASSISCH.ordinal());
+                RungeKuttaKlassisch rungekutta = (RungeKuttaKlassisch) this.integratoren.get(Integratoren.RUNGE_KUTTA_KLASSISCH.ordinal());
                 Vektor2D coordsRKK = rungekutta.getNewX(v, this.deltaT);
                 return coordsRKK;
             default:
                 return null;
         }
     }
-    
+
     private Vektor2D getDeltaV(Planet center, Planet moon) {
         switch (this.integrator) {
             case EULER:
-                Euler euler = (Euler)this.integratoren.get(Integratoren.EULER.ordinal());
+                Euler euler = (Euler) this.integratoren.get(Integratoren.EULER.ordinal());
                 Vektor2D vE = euler.getDeltaV(center.getCoords(), moon.getCoords(), center.getMass(), this.deltaT);
                 return vE;
             case LEAPFROG:
-                Leapfrog leapfrog = (Leapfrog)this.integratoren.get(Integratoren.LEAPFROG.ordinal());
+                Leapfrog leapfrog = (Leapfrog) this.integratoren.get(Integratoren.LEAPFROG.ordinal());
                 Vektor2D vL = leapfrog.getDeltaV(center.getCoords(), moon.getCoords(), center.getMass(), this.deltaT);
                 return vL;
             case RUNGE_KUTTA_KLASSISCH:
-                RungeKuttaKlassisch rungekutta = (RungeKuttaKlassisch)this.integratoren.get(Integratoren.RUNGE_KUTTA_KLASSISCH.ordinal());
+                RungeKuttaKlassisch rungekutta = (RungeKuttaKlassisch) this.integratoren.get(Integratoren.RUNGE_KUTTA_KLASSISCH.ordinal());
                 Vektor2D vRKK = rungekutta.getDeltaV(center.getCoords(), moon.getCoords(), center.getMass(), this.deltaT);
                 return vRKK;
             default:
                 return new Vektor2D();
         }
     }
-    
+
     public void setIntegrator(Integratoren integrator) {
         this.integrator = integrator;
     }
-    
+
     public void setDeltaT(double deltaT) {
         this.deltaT = deltaT;
     }
@@ -124,8 +145,8 @@ public class Rechenmodul implements Observer, Observable, Runnable {
     public void notifyObservers(Notifications type, String data) {
         Collection<Observer> values = this.observers.values();
         Object[] toArray = values.toArray();
-        for(Object temp : toArray) {
-            ((Observer)temp).notify(type, data);
+        for (Object temp : toArray) {
+            ((Observer) temp).notify(type, data);
         }
     }
 
@@ -135,7 +156,7 @@ public class Rechenmodul implements Observer, Observable, Runnable {
 
     @Override
     public void notify(Notifications type, String data) {
-        switch(type) {
+        switch (type) {
             case DELTA_CHANGE:
                 this.setDeltaT(Double.parseDouble(data));
                 break;
@@ -144,7 +165,7 @@ public class Rechenmodul implements Observer, Observable, Runnable {
 
     @Override
     public void sendPlanets(Notifications type, ArrayList<Planet> planets) {
-        switch(type) {
+        switch (type) {
             case UPDATE:
                 this.rechenschritt(planets);
                 break;
