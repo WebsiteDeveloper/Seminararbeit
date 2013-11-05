5bd6f0f64f91f684e3cc4eac1210208e9fa008f8
Bernhard Sirlinger <bernhard@sirlinger.de>
2013-11-05 19:07:37 +0100

    Cleanup

--- src/mss/integratoren/RungeKuttaKlassisch.java
+++ src/mss/integratoren/RungeKuttaKlassisch.java
@@ -31,8 +31,8 @@ import mss.util.Vektor2D;
  * @author Bernhard Sirlinger
  */
 public class RungeKuttaKlassisch implements Rechner {
-    public Vektor2D vn;
-    public Vektor2D wert1;
+    private Vektor2D vn;
+    private Vektor2D wert1;
 
     /**
      *
@@ -52,7 +52,6 @@ public class RungeKuttaKlassisch implements Rechner {
      * @param center
      * @param planet
      * @param masseCenter
-     * @param planetV
      * @param deltaT
      * @return
      */
@@ -80,37 +79,4 @@ public class RungeKuttaKlassisch implements Rechner {
         this.vn = vPlanet;
         return deltaV;
     }
-
-    /*
-    double deltaT_halbe = deltaT/2.0;
-        double einSechstel = 1.0 / 6.0;
-
-        Vektor2D ortsVektor = new Vektor2D(center.getX() - planet.getX(), center.getY() - planet.getY());
-        //Schritt y'
-        Vektor2D a = Util.getA(masseCenter, ortsVektor);
-        //System.out.println(a.toString());
-        Vektor2D vTemp1 = new Vektor2D(a.getX() * deltaT, a.getY() * deltaT);
-
-        //Schritt yA
-        Vektor2D coordsA = Vektor2D.add(planet, new Vektor2D(deltaT_halbe * vTemp1.getX(), deltaT_halbe * vTemp1.getY()));
-        Vektor2D ortsVektor1 = new Vektor2D(center.getX() - coordsA.getX(), center.getY() - coordsA.getY());
-        Vektor2D a1 = Util.getA(masseCenter, ortsVektor1);
-        Vektor2D vTemp2 = new Vektor2D(a1.getX() * deltaT, a1.getY() * deltaT);
-
-        //Schritt yB
-        Vektor2D coordsB = Vektor2D.add(planet, new Vektor2D(deltaT_halbe * vTemp2.getX(), deltaT_halbe * vTemp2.getY()));
-        Vektor2D ortsVektor2 = new Vektor2D(center.getX() - coordsB.getX(), center.getY() - coordsB.getY());
-        Vektor2D a2 = Util.getA(masseCenter, ortsVektor2);
-        Vektor2D vTemp3 = new Vektor2D(a2.getX() * deltaT, a2.getY() * deltaT);
-
-        //Schritt yC
-        Vektor2D coordsC = Vektor2D.add(planet, new Vektor2D(deltaT_halbe * vTemp3.getX(), deltaT_halbe * vTemp3.getY()));
-        Vektor2D ortsVektor3 = new Vektor2D(center.getX() - coordsC.getX(), center.getY() - coordsC.getY());
-        Vektor2D a3 = Util.getA(masseCenter, ortsVektor3);
-        Vektor2D vTemp4 = new Vektor2D(a3.getX() * deltaT, a3.getY() * deltaT);
-
-        Vektor2D v = Vektor2D.skalarMultiply(deltaT, Vektor2D.skalarMultiply(deltaT * einSechstel, Vektor2D.add(Vektor2D.add(a, Vektor2D.skalarMultiply(2, Vektor2D.add(a1, a2))), a3)));
-        //System.out.println(v.toString());
-        return v;
-    */
 }
